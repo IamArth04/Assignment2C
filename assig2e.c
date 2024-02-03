216a530c77ade4aaf4a2b1b3fe41680f17a24012
IamArth04 <111167184+IamArth04@users.noreply.github.com>
2024-02-04 00:40:30 +0530

    Update assig2e.c

--- assig2e.c
+++ assig2e.c
@@ -18,3 +18,27 @@ int main(){
     printf("Sum of the digits is %d",sum);
     return 0;
 }
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a=123;
+    int rem=0, sum=0;
+
+    rem=a%10; //123%10=3   "if we 123 ko mod% krte hai to sehphal aata hai AND divide/ krte hai to Bhaghphal aata hai
+    a=a/10; //123/10=12
+    sum=sum+rem; //0+3=3
+
+    rem=a%10; //12%10=2
+    a=a/10; //12/10=1
+    sum=sum+rem; //3+2=5
+
+    rem=a%10; //1%10=1
+    a=a/10; //1/10=0
+    sum=sum+rem; //5+1=6
+
+    cout<<sum<<endl;
+
+
+    return 0;
+}
